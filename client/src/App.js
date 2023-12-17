79b722deae8406aed28d84def8de6f3828e4e457
mrriina <mariaip12345678911@gmail.com>
2023-12-17 22:32:54 +0300

    imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/index';
+import { NavBar } from './components/index';
 import { routes } from './routes';
 
 
 function App() {
   return (
     <div className="h-screen bg-gray-100 dark:bg-gray-900 overflow-y-auto scrollHide">
-      <Navbar />
+      <NavBar />
       <Routes>
         {routes.map(({path, element}) =>
           <Route key={path} path={path} element={element} exact/>
